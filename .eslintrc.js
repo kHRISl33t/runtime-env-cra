a1c16b23bb36903752171fd2364ccd535cc99a25
kHRISl33t <kristof.ivancza@gmail.com>
2020-10-06 16:44:11 +0200

    added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'script',
+  },
+  rules: {
+    'no-useless-catch': 'off',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
